9e2d6f0941fee097f343c73bc81b2e49d571de41
Nazar <nazarzubyk@gmail.com>
2025-01-19 19:12:42 +0200

    matador moves

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,3 +1,31 @@
-export const Matador = () =>{
+import { useEffect } from "react"
+
+export const Matador = ({applause, setMatarodPosition, matadorPosition}) =>{
+
+
+
+    useEffect(() => {
+
+        const handleBullRun = (e)=>{
+       
+            if(matadorPosition === e.detail.position){
+                console.log('i am matador')
+                let newPosition = e.detail.position + 1
+                if(newPosition > 8){
+                    newPosition = 0
+                } 
+                setMatarodPosition(newPosition)
+            }
+        }
+
+        document.addEventListener('bullRun', handleBullRun)
+
+        return () => {
+            document.removeEventListener('bullRun', handleBullRun)
+        }
+    }, [])
+    
+
+    
     return <div>i am matador</div>
 }
