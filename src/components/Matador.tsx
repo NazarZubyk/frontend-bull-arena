fbeec37f71dfa7fa1ffe108ce853c608d8316d40
Nazar Zubyk <nazarzubyk@gmail.com>
2025-01-28 03:02:27 +0200

    by functional style realization

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,9 +1,34 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+
+const audio = 
+            [
+                new Audio('src/assets/suonds/mixkit-football-team-applause-509.wav'),
+                new Audio('src/assets/suonds/mixkit-cartoon-monkey-applause-103.wav'),
+                new Audio('src/assets/suonds/mixkit-football-team-applause-509.wav'),
+                new Audio('src/assets/suonds/mixkit-one-man-clapping-482.wav'),
+            ]
+        
 
 export const Matador = ({applause, setMatarodPosition, matadorPosition}) =>{
-
+    const [shouldRerender, setShouldRerender] = useState(false);
     const loggedRef = useRef(false)
+    const previousApplaus = useRef(applause)
 
+    useEffect(() => {
+
+        
+
+        if(previousApplaus.current !== 3 && previousApplaus.current === applause){
+            
+            audio[applause].play();
+            previousApplaus.current = applause;
+        }else if (previousApplaus.current === 3 && previousApplaus.current === applause){
+            
+            audio[applause].play();
+            setShouldRerender(true);
+        }
+        
+    }, [applause]);
 
     useEffect(() => {
 
@@ -33,5 +58,12 @@ export const Matador = ({applause, setMatarodPosition, matadorPosition}) =>{
     
 
     
-    return <div>i am matador</div>
+     
+    return <div>
+        <img 
+            src={`src/assets/images/bullfighter-matador-bullfighting-png-1797169806.png`} 
+            alt="matador" 
+            style={{ width: 200, height: 200 }} 
+        />
+    </div>
 }
